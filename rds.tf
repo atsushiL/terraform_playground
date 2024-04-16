1ae94a3ad7cf08cdd820e76a8c28bce3c51bce43
atsushiL <guratsushi@gmail.com>
2024-04-16 18:57:13 +0900

    RDS リソースの部分をコメントアウト

--- rds.tf
+++ rds.tf
@@ -45,56 +45,56 @@ resource "random_string" "db_password" {
   special = false
 }
 
-resource "aws_db_instance" "mysql_standalone" {
-  # DB エンジン情報
-  engine         = "mysql"
-  engine_version = "8.0.32"
+# resource "aws_db_instance" "mysql_standalone" {
+#   # DB エンジン情報
+#   engine         = "mysql"
+#   engine_version = "8.0.32"
 
-  # DB インスタンス名
-  identifier = "${var.projectname}-${var.environment}-mysql-standalone"
+#   # DB インスタンス名
+#   identifier = "${var.projectname}-${var.environment}-mysql-standalone"
 
-  # DB 秘匿情報
-  username = "admin"
-  password = random_string.db_password.result
+#   # DB 秘匿情報
+#   username = "admin"
+#   password = random_string.db_password.result
 
-  # DB インスタンスのスペック
-  instance_class = "db.m5d.large"
+#   # DB インスタンスのスペック
+#   instance_class = "db.m5d.large"
 
-  # ストレージ
-  allocated_storage     = 20
-  max_allocated_storage = 50
-  storage_type          = "gp2"
-  storage_encrypted     = false
+#   # ストレージ
+#   allocated_storage     = 20
+#   max_allocated_storage = 50
+#   storage_type          = "gp2"
+#   storage_encrypted     = false
 
-  # ネットワーク
-  multi_az               = false
-  availability_zone      = "ap-northeast-1a"
-  db_subnet_group_name   = aws_db_subnet_group.mysql_standalone-subnetgroup.name
-  vpc_security_group_ids = [aws_security_group.rds_sg.id]
-  publicly_accessible    = false
-  port                   = 3306
+#   # ネットワーク
+#   multi_az               = false
+#   availability_zone      = "ap-northeast-1a"
+#   db_subnet_group_name   = aws_db_subnet_group.mysql_standalone-subnetgroup.name
+#   vpc_security_group_ids = [aws_security_group.rds_sg.id]
+#   publicly_accessible    = false
+#   port                   = 3306
 
-  # DB 名
-  name                 = "tastylog"
-  parameter_group_name = aws_db_parameter_group.mysql_standalone_parametergroup.name
-  option_group_name    = aws_db_option_group.mysql_standalone_optiongroup.name
+#   # DB 名
+#   name                 = "tastylog"
+#   parameter_group_name = aws_db_parameter_group.mysql_standalone_parametergroup.name
+#   option_group_name    = aws_db_option_group.mysql_standalone_optiongroup.name
 
-  # メンテナンス
-  backup_window              = "04:00-05:00"
-  backup_retention_period    = 7
-  maintenance_window         = "Mon:05:00-Mon:08:00"
-  auto_minor_version_upgrade = false
+#   # メンテナンス
+#   backup_window              = "04:00-05:00"
+#   backup_retention_period    = 7
+#   maintenance_window         = "Mon:05:00-Mon:08:00"
+#   auto_minor_version_upgrade = false
 
-  # インスタンス・データ削除保護
-  deletion_protection = false
-  skip_final_snapshot = true
+#   # インスタンス・データ削除保護
+#   deletion_protection = false
+#   skip_final_snapshot = true
 
-  # 検証環境であるため、変更を即時反映させる
-  apply_immediately = true
+#   # 検証環境であるため、変更を即時反映させる
+#   apply_immediately = true
 
-  tags = {
-    Name        = "${var.projectname}-${var.environment}-mysql-standalone"
-    ProjectName = var.projectname
-    Environment = var.environment
-  }
-}
+#   tags = {
+#     Name        = "${var.projectname}-${var.environment}-mysql-standalone"
+#     ProjectName = var.projectname
+#     Environment = var.environment
+#   }
+# }
