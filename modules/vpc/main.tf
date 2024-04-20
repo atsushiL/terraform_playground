946b560894aceb2f238a8b122f7ce334c138763b
atsushiL <guratsushi@gmail.com>
2024-04-20 19:00:48 +0900

    ファイル構成の修正

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -2,9 +2,10 @@
 # VPC
 # ----------------------------------
 resource "aws_vpc" "vpc" {
-  cidr_block           = var.vpc_cidr
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+  cidr_block                       = var.vpc_cidr
+  enable_dns_support               = true
+  enable_dns_hostnames             = true
+  assign_generated_ipv6_cidr_block = false
   tags = {
     Name = "${var.projectname}-${var.environment}-vpc"
   }
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,19 +1,5 @@
-variable "projectname" {
-  type        = string
-  description = "Enter the project name."
-  default     = "demo-terraform"
-}
-
-variable "environment" {
-  type        = string
-  description = "Enter the Environment."
-  default     = "dev"
-}
-
-variable "vpc_cidr" {
-  type        = string
-  description = "Enter the vpc cidr."
-  default     = "10.0.0.0/16"
-}
+variable "projectname" {}
+variable "environment" {}
+variable "vpc_cidr" {}
 
 
