1470a3994c4c9ae766796de9f83d8db41dc41d62
atsushiL <guratsushi@gmail.com>
2024-04-23 00:18:16 +0900

    不要ファイルの削除および module 配下に README ファイル配置

--- aws/modules/vpc/main.tf
+++ aws/modules/vpc/main.tf
@@ -1,4 +1,22 @@
 # ------------------------------------------
+# data
+# ------------------------------------------
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+# ------------------------------------------
+# locals
+# ------------------------------------------
+locals {
+  account_id = data.aws_caller_identity.current.account_id
+  region     = data.aws_region.current.name
+  az_suffix1 = split("-", var.availability_zone1)[2]
+  az_suffix2 = split("-", var.availability_zone2)[2]
+  is_prd     = var.environment == "prd"
+}
+
+
+# ------------------------------------------
 # VPC
 # ------------------------------------------
 resource "aws_vpc" "vpc" {
